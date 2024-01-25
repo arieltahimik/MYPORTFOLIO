c7a5982977ca6552d21aec5d881aea19f7598be3
arista96 <116620105+arista96@users.noreply.github.com>
2024-01-24 20:06:19 -0600

    Modified BMI Calculator

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -5,6 +5,32 @@ export default class BmiCalculator extends LightningElement {
   weight = ''
   bmiValue = ''
   result = ''
+
+  bmi = '' //bmi for US & Metric
+  heightUnit ='(inches)' //placeholder
+  weightUnit ='(pounds)' //placeholder
+  unit = 'in_lbs'  //in_lbs or cm_kg
+
+  // which radio button? in/lbs or cm/kg
+  radioHandle(event){
+    const {value} = event.target
+    console.log('value', value)
+    if(value === 'in_lbs') {
+      // this.heightUnit = 'Enter your height in inches'
+      this.heightUnit = '(inches)'
+      this.weightUnit = '(pounds)'
+      this.unit = value
+    } else if(value === 'cm_kg') {
+      // this.heightUnit = 'Enter your height in centimeters'
+      this.heightUnit = '(centimeters)'
+      this.weightUnit = '(kilograms)'
+      this.unit = value
+    } else {
+      alert('Please choose a unit.')
+    }
+  }
+
+
   inputHandler(event){
     const {name, value} = event.target
     if(name === "height"){
@@ -24,12 +50,21 @@ export default class BmiCalculator extends LightningElement {
     this.calculate()
   }
 
-  calculate(){
-    //BMI = weight in KG / (height in m * height in m)
-    let height = Number(this.height)/100;
-    let bmi = Number(this.weight) /(height*height);
+  calculate() {
+    if (this.unit === 'in_lbs') {
+      let height = Number(this.height);
+      let weight = Number(this.weight);
+      // let bmi = (weight /(height*height)) * 703;
+      this.bmi = (weight /(height*height)) * 703;
+    } else {
+      //BMI = weight in KG / (height in m * height in m)
+      let height = Number(this.height)/100;
+      //let bmi = Number(this.weight) /(height*height);
+      this.bmi = Number(this.weight) /(height*height);
+    }
     
-    this.bmiValue = Number(bmi.toFixed(2))
+    //this.bmiValue = Number(bmi.toFixed(2))
+    this.bmiValue = Number(this.bmi.toFixed(2))
 
     if(this.bmiValue <18.5){
       this.result = "Underweight"
@@ -50,5 +85,8 @@ export default class BmiCalculator extends LightningElement {
     this.weight = ''
     this.bmiValue = ''
     this.result = ''
+    //blank the placeholder
+    this.heightUnit='(inches)'
+    this.weightUnit='(pounds)'
   }
 }
